8ae385d3c661f8b570c6cd5aa8beac35b9750a76
FantasyNJ <ningjie1993@gmail.com>
2022-05-29 15:52:37 +0800

    change banner img src

--- js/index.js
+++ js/index.js
@@ -76,7 +76,7 @@
     var oBanner=document.getElementById('g-banner');
     var oImage=new Image();
     var arrBac=['#100805','#0D1A22','#CFCECA','#171E28','#27010E','#D4D4D2','#11100C','#E9F3F5'];
-    var arrImg=['img/banner/banner-1.jpg','img/banner/banner-2.jpg','img/banner/banner-3.jpg','img/banner/banner-4.jpg','img/banner/banner-5.jpg','img/banner/banner-6.jpg','img/banner/banner-7.jpg','img/banner/banner-8.jpg'];
+    var arrImg=['img/banner/1.jpg','img/banner/2.jpg','img/banner/3.jpg','img/banner/4.jpg','img/banner/5.jpg','img/banner/6.jpg','img/banner/7.jpg','img/banner/8.jpg'];
     var oImg=document.getElementById('img');
     var oWrap=getElementsByClassName(oBanner,'wrap')[0];
     var aI=oWrap.getElementsByTagName('i');
